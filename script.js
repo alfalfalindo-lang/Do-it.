5f0fc00bb12072637e667635cc6303fbc501dd42
alfalfalindo-lang <alfalfalindo@gmail.com>
2026-02-20 14:47:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,24 @@ let tarefas = [];
 let metas = [];
 let modo = "tarefas";
 
+function atualizarData() {
+  let hoje = new Date();
+  document.getElementById("dataHoje").innerText =
+    hoje.toLocaleDateString("pt-BR");
+}
+atualizarData();
+
 function adicionar() {
   let texto = document.getElementById("texto").value;
   let prioridade = document.getElementById("prioridade").value;
   let data = document.getElementById("data").value;
 
-  if (texto === "") return;
+  if (!texto) return;
 
-  let item = {
-    texto,
-    prioridade,
-    data,
-    concluida: false
-  };
+  let item = { texto, prioridade, data, concluida: false };
 
-  if (modo === "metas") {
-    metas.push(item);
-  } else {
-    tarefas.push(item);
-  }
+  if (modo === "metas") metas.push(item);
+  else tarefas.push(item);
 
   document.getElementById("texto").value = "";
   renderizar();
@@ -39,21 +38,14 @@ function renderizar() {
 
     div.innerHTML = `
       ${item.texto}
-      <span class="prioridade ${item.prioridade}">
-        ${item.prioridade}
-      </span>
+      <span class="prioridade ${item.prioridade}">${item.prioridade}</span>
       ${item.data ? "<small> - " + item.data + "</small>" : ""}
     `;
 
     div.onclick = () => {
       item.concluida = !item.concluida;
-
-      // Se estiver na aba incompletas e marcar como concluída
-      if (modo === "incompletas" && item.concluida) {
-        renderizarIncompletas();
-      } else {
-        renderizar();
-      }
+      if (modo === "incompletas") renderizarIncompletas();
+      else renderizar();
     };
 
     lista.appendChild(div);
@@ -79,25 +71,29 @@ function renderizarIncompletas() {
   let lista = document.getElementById("lista");
   lista.innerHTML = "";
 
-  tarefas
-    .filter(t => !t.concluida)
-    .forEach(item => {
-      let div = document.createElement("div");
-      div.className = "item";
+  tarefas.filter(t => !t.concluida).forEach(item => {
+    let div = document.createElement("div");
+    div.className = "item";
 
-      div.innerHTML = `
-        ${item.texto}
-        <span class="prioridade ${item.prioridade}">
-          ${item.prioridade}
-        </span>
-        ${item.data ? "<small> - " + item.data + "</small>" : ""}
-      `;
+    div.innerHTML = `
+      ${item.texto}
+      <span class="prioridade ${item.prioridade}">${item.prioridade}</span>
+      ${item.data ? "<small> - " + item.data + "</small>" : ""}
+    `;
 
-      div.onclick = () => {
-        item.concluida = true;
-        renderizarIncompletas();
-      };
+    div.onclick = () => {
+      item.concluida = true;
+      renderizarIncompletas();
+    };
 
-      lista.appendChild(div);
-    });
+    lista.appendChild(div);
+  });
+}
+
+function abrirNotas() {
+  document.getElementById("notasModal").style.display = "flex";
+}
+
+function fecharNotas() {
+  document.getElementById("notasModal").style.display = "none";
 }
