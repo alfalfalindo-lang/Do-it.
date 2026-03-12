c8d1eb8c92e36d4b8fb99c6bd79a85b0e9d0ba54
alfalfalindo-lang <alfalfalindo@gmail.com>
2026-03-12 08:55:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 let tarefas=JSON.parse(localStorage.getItem("tarefas"))||[];
 let metas=JSON.parse(localStorage.getItem("metas"))||[];
 let postits=JSON.parse(localStorage.getItem("postits"))||[];
-let paginas=JSON.parse(localStorage.getItem("paginas"))||[""];
 
 let modo="tarefas";
-let corSelecionada="vermelha";
+let tipoCriacao="tarefa";
 
-/* data */
+let corSelecionada="#ff3b30";
+
+/* DATA */
 
 const hoje=new Date();
 
 mesTopo.innerText=hoje.toLocaleDateString("pt-BR",{month:"long"});
 diaTopo.innerText=hoje.getDate();
 
-/* reset metas mês */
+/* RESET METAS MENSAL */
 
+const mesAtual=hoje.getMonth();
 const mesSalvo=localStorage.getItem("mesMetas");
 
-if(mesSalvo!=hoje.getMonth()){
+if(mesSalvo!=mesAtual){
 
 metas=[];
-localStorage.setItem("mesMetas",hoje.getMonth());
+localStorage.setItem("mesMetas",mesAtual);
 
 }
 
-/* salvar */
+/* SALVAR */
 
 function salvar(){
 
 localStorage.setItem("tarefas",JSON.stringify(tarefas));
 localStorage.setItem("metas",JSON.stringify(metas));
 localStorage.setItem("postits",JSON.stringify(postits));
-localStorage.setItem("paginas",JSON.stringify(paginas));
 
 }
 
-/* render */
+/* RENDER */
 
 function render(){
 
@@ -47,10 +48,14 @@ if(modo==="tarefas")lista=tarefas;
 if(modo==="metas")lista=metas;
 
 if(modo==="incompletas"){
+
 lista=[
+
 ...tarefas.filter(t=>!t.feita),
 ...metas.filter(t=>!t.feita)
+
 ];
+
 }
 
 lista.forEach(t=>{
@@ -62,7 +67,7 @@ div.className="tarefa";
 if(t.feita)div.classList.add("feita");
 
 div.innerHTML=`
-<div class="bolinha-tarefa" style="background:${t.cor}"></div>
+<div class="bolinha" style="background:${t.cor}"></div>
 <div>${t.titulo}</div>
 `;
 
@@ -84,52 +89,78 @@ listaTarefas.appendChild(div);
 
 render();
 
-/* criar tarefa */
+/* CRIAR */
 
 criar.onclick=()=>{
 
 let obj={
+
 titulo:titulo.value,
-data:data.value,
 cor:corSelecionada,
 feita:false
+
 };
 
-if(modo==="metas")metas.push(obj);
+if(tipoCriacao==="meta")metas.push(obj);
 else tarefas.push(obj);
 
 salvar();
+
 render();
 
 modal.style.display="none";
 
 };
 
-/* cores */
+/* MODAL */
+
+novoBtn.onclick=()=>{
+
+tipoCriacao="tarefa";
+
+tituloModal.innerText="nova tarefa";
+
+modal.style.display="flex";
+
+};
+
+novaMetaBtn.onclick=()=>{
+
+tipoCriacao="meta";
+
+tituloModal.innerText="nova meta";
+
+modal.style.display="flex";
+
+};
+
+cancelar.onclick=()=>modal.style.display="none";
+
+document.querySelector(".fechar").onclick=()=>modal.style.display="none";
+
+/* CORES */
 
 document.querySelectorAll(".cor").forEach(btn=>{
 
-btn.onclick=()=>corSelecionada=btn.dataset.cor;
+btn.onclick=()=>{
+
+corSelecionada=btn.dataset.cor;
+
+};
 
 });
 
-/* modal */
-
-novoBtn.onclick=()=>modal.style.display="flex";
-cancelar.onclick=()=>modal.style.display="none";
-document.querySelector(".fechar").onclick=()=>modal.style.display="none";
-
-/* mudar modo */
+/* MODOS */
 
 tarefasBtn.onclick=()=>{modo="tarefas";render();}
 metasBtn.onclick=()=>{modo="metas";render();}
 incompletasBtn.onclick=()=>{modo="incompletas";render();}
 
-/* postit */
+/* POST IT */
 
 postitBtn.onclick=()=>{
 
-let obj={top:"150px",left:"300px",texto:""};
+let obj={top:"150px",left:"350px",texto:""};
 
 postits.push(obj);
 
@@ -152,7 +183,7 @@ div.innerHTML=`<textarea>${p.texto}</textarea>`;
 
 document.body.appendChild(div);
 
-/* drag */
+/* DRAG */
 
 div.onmousedown=e=>{
 
@@ -173,16 +204,18 @@ document.onmouseup=()=>{
 
 document.onmousemove=null;
 
-/* lixeira */
+/* APAGAR NA LIXEIRA */
 
 let lixo=lixeira.getBoundingClientRect();
 let post=div.getBoundingClientRect();
 
 if(
+
 post.right>lixo.left &&
 post.left<lixo.right &&
 post.bottom>lixo.top &&
 post.top<lixo.bottom
+
 ){
 
 div.remove();
